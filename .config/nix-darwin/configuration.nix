399330f2155674c3107ba70eb61ef6daa54d68fa
RyushiAok <55625375+RyushiAok@users.noreply.github.com>
2026-06-10 16:28:22 +0900

    nix darwin

--- .config/nix-darwin/configuration.nix
+++ .config/nix-darwin/configuration.nix
@@ -31,28 +31,48 @@
 
   # https://github.com/nix-darwin/nix-darwin/blob/master/modules/examples/flake/flake.nix
   environment.systemPackages = with pkgs; [
-    mise
-    zellij
-
     # aws
     awscli2
     aws-vault
     ssm-session-manager-plugin
 
+    # shell
+    mise
+    starship
+    direnv
+    fd
+    fzf
+    ripgrep
+    yazi
+    zellij
+    zsh-autosuggestions
+    zsh-syntax-highlighting
+
     # git
     git
     git-lfs
     gh
+    ghq
     lazygit
     lefthook
-    ghq
-    fzf
+    delta
 
-    # shell
-    starship
+    # terraform / infra
+    terraform
+    terraform-ls
+    atlas
 
-    # network
-    tailscale
+    # editor / tools
+    neovim
+    helix
+    aria2
+    marp-cli
+    mint
+    mysql84
+    pkg-config
+
+    # # network
+    # tailscale
 
     # nix
     nixd
@@ -65,13 +85,18 @@
 
   programs.zsh = {
     enable = true;
-    shellInit = ''
+    interactiveShellInit = ''
       eval "$(mise activate zsh)"
       eval "$(mise activate --shims)"
-      eval "$(direnv hook zsh)"
       eval "$(starship init zsh)"
-      eval "$(rbenv init - zsh)"
-
+      # eval "$(rbenv init - zsh)"
+      if [ -x /opt/homebrew/bin/brew ]; then
+        export HOMEBREW_PREFIX="/opt/homebrew"
+        export HOMEBREW_CELLAR="/opt/homebrew/Cellar"
+        export HOMEBREW_REPOSITORY="/opt/homebrew"
+        case ":$PATH:" in *":/opt/homebrew/bin:"*) ;; *) export PATH="$PATH:/opt/homebrew/bin" ;; esac
+        case ":$PATH:" in *":/opt/homebrew/sbin:"*) ;; *) export PATH="$PATH:/opt/homebrew/sbin" ;; esac
+      fi
       export GHQ_ROOT="$HOME/repo"
       ghq-jump-widget() {
         local repo
@@ -84,23 +109,32 @@
 
       unsetopt correct correctall
     '';
+    enableAutosuggestions = true;
+    enableSyntaxHighlighting = true;
   };
 
   homebrew = {
     enable = true;
     user = user;
+    onActivation = {
+      cleanup = "uninstall";
+      extraFlags = [ "--force-cleanup" ];
+    };
     casks = [
-      "visual-studio-code"
+      "beekeeper-studio"
+      "claude-code"
       "cursor"
-      "xcodes"
-      "raycast"
-      "warp"
+      "drawio"
+      "figma"
+      "ghostty"
+      "gitify"
+      "karabiner-elements"
+      "notion"
+      "session-manager-plugin"
+      "visual-studio-code"
+      "xcodes-app"
     ];
-    brews = [
-      "mint"
-      "neovim"
-      "aria2"
-    ];
+    brews = [ ];
   };
 
   fonts = {
